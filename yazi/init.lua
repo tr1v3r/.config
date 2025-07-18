533a5bf4ef47bb9c620292204ff9b4bc66f08de5
tr1v3r <churiver@outlook.com>
2025-07-18 16:40:37 +0800

    feat(yazi): upgrade yazi plugins

--- yazi/init.lua
+++ yazi/init.lua
@@ -1,11 +1,14 @@
 -- Docs: https://yazi-rs.github.io/
-require("yaziline"):setup({
-	separator_style = "curvy",
-	select_symbol = "",
-	yank_symbol = "󰆐",
-	filename_max_length = 24, -- trim when filename > 24
-	filename_trim_length = 6, -- trim 6 chars from both ends
-})
+
+-- https://github.com/llanosrocas/yaziline.yazi
+-- require("yaziline"):setup({
+-- 	separator_style = "curvy",
+-- 	select_symbol = "",
+-- 	yank_symbol = "󰆐",
+-- 	filename_max_length = 24, -- trim when filename > 24
+-- 	filename_trim_length = 6, -- trim 6 chars from both ends
+-- })
+
 require("starship"):setup({
 	config_file = "~/.config/yazi/starship.toml",
 })
